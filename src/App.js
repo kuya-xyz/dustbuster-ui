8bd5b89a8939f4e7d750c3a7945dda41a90eaf4a
kuya-xyz <will@kuya.xyz>
2025-11-20 12:24:07 -0800

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{textAlign:'center', marginTop:'200px', fontSize:'48px'}}>
+      Dustbuster.ai coming Tuesday 4:20<br/>
+      <span style={{fontSize:'100px'}}>🧹</span>
+    </div>
+  );
+}
+
+export default App;
